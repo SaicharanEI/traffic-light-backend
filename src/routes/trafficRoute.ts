667f17ddc8dfc5d117f327b9e251b72d0e07949a
Saicharan <saicharan.g@evolutioncloud.in>
2024-06-27 12:20:55 +0530

    fixed bugs

--- src/routes/trafficRoute.ts
+++ src/routes/trafficRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { AddTrafficLight, DeleteTrafficLight, deleteSchedule, getTrafficLight, getTrafficLightsList, updateTrafficLight, updateTrafficLightCurrentColor } from "../controllers/trafficControllers";
+import { AddTrafficLight, DeleteTrafficLight, deleteSchedule, getTrafficLight, getTrafficLightsList, updateTrafficLight } from "../controllers/trafficControllers";
 import { errorHandler } from "../error-handler";
 
 export const trafficRoute: Router = Router();
@@ -8,7 +8,6 @@ trafficRoute.post("/addtrafficlight", errorHandler(AddTrafficLight))
 trafficRoute.put("/updatetrafficlight/:id", errorHandler(updateTrafficLight))
 trafficRoute.get("/trafficlights", errorHandler(getTrafficLightsList))
 trafficRoute.get("/trafficlight/:id", errorHandler(getTrafficLight))
-trafficRoute.put("/updatetrafficlightcolor/:id", errorHandler(updateTrafficLightCurrentColor))
+// trafficRoute.put("/updatetrafficlightcolor/:id", errorHandler(updateTrafficLightCurrentColor))
 trafficRoute.delete("/deleteschedule/:id", errorHandler(deleteSchedule))
 trafficRoute.delete(`/deletetrafficlight/:id`, errorHandler(DeleteTrafficLight))
-trafficRoute.get("/traffic", (req,res) => {res.send("Hello World from traffic route")})
